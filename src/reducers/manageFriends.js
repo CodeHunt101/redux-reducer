629046d6c6528b370c2f07492359936835c52d07
Harold Torres Marino <haroldtm55@gmail.com>
2021-11-17 17:58:30 +1100

    Lab completed

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends:[]}, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const copyOfFriends = [...state.friends]
+      const friendToRemoveIndex = copyOfFriends.indexOf(copyOfFriends.find(friend => friend.id === action.id))
+      copyOfFriends.splice(friendToRemoveIndex, 1)
+      return {friends: copyOfFriends}
+    default:
+      return state
+  }
 }
